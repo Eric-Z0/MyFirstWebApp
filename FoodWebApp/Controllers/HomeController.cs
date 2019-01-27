7c43eae1115ad5a9ba316ab2accd5d2de5f5887f
Eric-Z0 <ericzhuang603@gmail.com>
2019-01-26 21:31:24 -0500

    Created a drift version of home page having most of the components; Also, updated the the version of Bootstrap and JQuery

--- FoodWebApp/Controllers/HomeController.cs
+++ FoodWebApp/Controllers/HomeController.cs
@@ -13,16 +13,37 @@ namespace FoodWebApp.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Message()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "My Message Page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult MAP()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "My MAP Page.";
+
+            return View();
+        }
+
+        public ActionResult Weather()
+        {
+            ViewBag.Message = "My Weather Page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "My Login Page.";
+
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            ViewBag.Message = "My Signup Page.";
 
             return View();
         }
